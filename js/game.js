68d34b7a4381c53d31ca0579b4049ed7b09461ad
OchoaUnicen <ignacio.ochoa.unicen@gmail.com>
2020-01-27 23:17:50 -0300

    se implementaron imagenes x/o para jugadores - se implemento funcion para detectar donde se hace click dentro del canvas - se implemento funcion para redibujar el canvas 60 veces por segundo

--- js/game.js
+++ js/game.js
@@ -4,6 +4,18 @@ document.addEventListener('DOMContentLoaded', function () {
     let context = canvas.getContext("2d");
 
 
+
+    let frame = window.requestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
+
+
+
+
+
+   
+
     function dibujarLinea(x1, y1, x2, y2) {     
         context.beginPath();
         context.moveTo(x1, y1);
@@ -12,11 +24,94 @@ document.addEventListener('DOMContentLoaded', function () {
         context.stroke();    
     }
 
+    
+   
 
-    dibujarLinea(200, 0 , 200 , 600);
-    dibujarLinea(400, 0 , 400 , 600);
-    dibujarLinea(0, 200 , 600 , 200);
-    dibujarLinea(0, 400 , 600 , 400);
+    let imagen_x = new Image();
+    imagen_x.src = "./img/x.png";
+
+
+    function dibujarX () {    
+  
+      context.drawImage(imagen_x, 50 , 50, imagen_x.naturalWidth, imagen_x.naturalHeight);
+    }
+
+
+    dibujarX();
+
+   
+   
+    
+    console.log(imagen_x);
+
+
+    
+
+    let Usuarios = {
+
+      "usuario_1": {
+        "username": "Guido",
+        "password": "Pola"
+      },
+      "usuario_2": {
+        "username": "Nacho",
+        "password": "Master"
+      },
+
+      "usuario_3": {
+        "username": "Sebinsky",
+        "password": "hackerpro911"
+      }
+
+    }
+
+
+
+    //--------------------------------------------------------------------
+    canvas.addEventListener("click", function (evt) {
+      var mousePos = getMousePos(canvas, evt);
+
+      console.log("clicked");
+      console.log(mousePos.x + ',' + mousePos.y);   
+      
+      
+
+      //aca va logica de clickear para colocar x/o en cuadro
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+  }, false);
+  
+  //Get Mouse Position
+  function getMousePos(canvas, evt) {
+      var rect = canvas.getBoundingClientRect();
+      return {
+          x: evt.clientX - rect.left,
+          y: Math.floor(evt.clientY - rect.top)
+      };
+  }
+ //--------------------------------------------------------------------
+
+
+
+
+
 
 
 
@@ -27,15 +122,42 @@ document.addEventListener('DOMContentLoaded', function () {
     let direccion_url = "http://web-unicen.herokuapp.com/api/groups/26/tateti";
 
      //el json dentro de thing
-     let info_cargada = {
-        "thing": {
-          "nombreItem": nombre_item,
-          "precioGe": precioge_item
-        }
-      };
+    
+    
+     //  let info_cargada = {
+    //     "thing": {
+    //       "nombreItem": nombre_item,
+    //       "precioGe": precioge_item
+    //     }
+    //   };
 
 
 
+
+  function tiempo() {  
+    frame(tiempo);
+
+
+
+    context.clearRect(0, 0, canvas.width, canvas.heigh);
+    dibujarLinea(200, 0 , 200 , 600);
+    dibujarLinea(400, 0 , 400 , 600);
+    dibujarLinea(0, 200 , 600 , 200);
+    dibujarLinea(0, 400 , 600 , 400);
+
+
+
+    //
+    dibujarX();
+    //
+
+
+
+
+  }
+
+   
+  tiempo();
 
 
 
